43769fc12a3d303f5f4311f9216c7033832ed4b9
Alonso <alonsonovik@gmail.com>
2021-03-04 00:48:09 +0500

    canceling the second education list creation

--- education.py
+++ education.py
@@ -9,11 +9,3 @@ for line in f:
         education_list_1.append(list_string[2][1:-1])
 education_list_1.sort()
 education_list_2.append(education_list_1[0]) 
-for i in range(len(education_list_1)):
-    if i+1==len(education_list_1):
-        break
-    elif education_list_1[i+1]!=education_list_1[i]:
-        education_list_2.append(education_list_1[i+1])
-    else:
-        continue
-print(education_list_2)
